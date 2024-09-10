8ff9fa3e3a04111c76a9ca59d3e9862ed665cbdc
Julian97s <124013438+Julian97s@users.noreply.github.com>
2024-09-10 14:37:12 -0400

    printing color_passcode with a null at first

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        //first ill have to create a passcode when i run the initial program
+        Passcode new_passcode = new Passcode();
+
+        System.out.println(new_passcode);
+
+
+    }
+}
---
+++ Passcode.java
@@ -0,0 +1,29 @@
+public class Passcode {
+
+    GuessCell[] pass;
+
+    public Passcode(){
+        pass = new GuessCell[4]; 
+        setPasscode();
+    }
+
+
+    // setPasscode will populate the 4 array slots with random colors
+    private void setPasscode(){
+        for (int i=0; i<pass.length; i++){
+            pass[i] = GuessCell.createRandomCell();
+        }
+    }
+
+    @Override
+    public String toString(){
+        String color_pass = null;
+        for (GuessCell cell : pass) {
+            color_pass += cell + "\t";
+        }
+        return color_pass;
+    }
+
+    // call each GuesCell to print it inside a for each loop 
+
+}
